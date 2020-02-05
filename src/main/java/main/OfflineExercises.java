d389ed9fd291c893fc5e44ef481ebf5e8d0fc0ec
lmcg05 <60017111+lmcg05@users.noreply.github.com>
2020-02-05 10:30:20 +0000

    Add files via upload

--- src/main/java/main/OfflineExercises.java
+++ src/main/java/main/OfflineExercises.java
@@ -1,8 +1,9 @@
 package main;
 
-public class OfflineExercises {
+import java.util.ArrayList;
+import java.util.List;
 
-	private boolean atDev;
+public class OfflineExercises {
 
 	// Given a string, return a string where
 	// for every char in the original string,
@@ -13,9 +14,15 @@ public class OfflineExercises {
 	// multChar("Hi-There") → "HHHiii---TTThhheeerrreee"
 
 	public String multChar(String input) {
-		return "";
+		String result = "";
+		for (int i = 0; i < input.length(); i++) {
+			for (int j = 0; j < 3; j++) {
+				result = result + input.charAt(i);
+			}
+		}
+		return result;
 	}
-	
+
 	// Return the string (backwards) that is between the first and last appearance
 	// of "bert"
 	// in the given string, or return the empty string "" if there is not 2
@@ -28,7 +35,18 @@ public class OfflineExercises {
 	// getBert("xxbeRTyy") → ""
 
 	public String getBert(String input) {
-		return "";
+		String x = "";
+		if (input.toLowerCase().contains("bert")) {
+			if (input.toLowerCase().substring(input.toLowerCase().indexOf("bert") + 4).contains("bert")) {
+				String y = input.toLowerCase().substring(input.toLowerCase().indexOf("bert") + 4,
+						input.toLowerCase().lastIndexOf("bert"));
+				for (int i = y.length() - 1; i > -1; i--) {
+					x += y.toCharArray()[i];
+				}
+			}
+		}
+		return x;
+
 	}
 
 	// Given three ints, a b c, one of them is small, one is medium and one is
@@ -43,27 +61,32 @@ public class OfflineExercises {
 	// evenlySpaced(4, 60, 9) → false
 
 	public boolean evenlySpaced(int a, int b, int c) {
-		
+		List<Integer> num = new ArrayList<Integer>();
+		num.add(a);
+		num.add(b);
+		num.add(c);
+		num.sort(null);
+		int checknum1 = num.get(2) - num.get(1);
+		int checknum2 = num.get(1) - num.get(0);
+		if (checknum1 == checknum2) {
+			return true;
+		} else {
+
+		}
 		return false;
 	}
 
-	// Given a string and an int n, return a string that removes n letters from the 'middle' of the string.
-	// The string length will be at least n, and be odd when the length of the input is odd.
+	// Given a string and an int n, return a string that removes n letters from the
+	// 'middle' of the string.
+	// The string length will be at least n, and be odd when the length of the input
+	// is odd.
 
 	// nMid("Hello", 3) → "Ho"
 	// nMid("Chocolate", 3) → "Choate"
 	// nMid("Chocolate", 1) → "Choclate"
 
 	public String nMid(String input, int a) {
-		String str = "Hello";
-		System.out.println("contains sequence 'ell' :"+ str +str.contains("ell"));
-		
-		String str1 = "Chocolate";
-		System.out.println("contains sequence 'col' :"+ str1 + str1.contains("col"));
-		
-		String str2 = "Chocolate";
-		System.out.println("contains sequence 'o' :"+ str2 +str2.contains("o"));
-		return ("String str, String str1, String str2");
+		return "";
 	}
 
 	// Given a string, return true if it ends in "dev". Ignore Case
@@ -73,21 +96,10 @@ public class OfflineExercises {
 	// endsDev("everoyonehatesdevforreal") → false
 	// endsDev("devisnotcool") → false
 
-	
 	public boolean endsDev(String input) {
-	
-		if (input.endsWith("endswith character 'dev':" + input.endsWith("dev"))) { //ive improve this from old one.
-	
-	return true;
-		} else {
-		return false;
-}
+
+		return input.toLowerCase().endsWith("dev");
 	}
-
-
-
-
-		
 
 	// Given a string, return the length of the largest "block" in the string.
 	// A block is a run of adjacent chars that are the same.
@@ -97,50 +109,58 @@ public class OfflineExercises {
 	// superBlock("") → 0
 
 	public int superBlock(String input) {
-		
 		return -1;
 
 	}
-	
-	//given a string - return the number of times "am" appears in the String ignoring case -
-	// BUT ONLY WHEN the word "am" appears without being followed or proceeded by other letters
+
+	// given a string - return the number of times "am" appears in the String
+	// ignoring case -
+	// BUT ONLY WHEN the word "am" appears without being followed or proceeded by
+	// other letters
 	//
-	//amISearch("Am I in Amsterdam") → 1
-	//amISearch("I am in Amsterdam am I?") → 2
-	//amISearch("I have been in Amsterdam") → 0
+	// amISearch("Am I in Amsterdam") → 1
+	// amISearch("I am in Amsterdam am I?") → 2
+	// amISearch("I have been in Amsterdam") → 0
 
 	public int amISearch(String arg1) {
-		char[]array = "believe".split("?!");     
-		for(int i = 0; i < arg1.length(); i++) {
-			if(array[i] == "am" && char[i+1] == "am") { 
-				
-	
+		int count = 0;
+		String lower = arg1.toLowerCase();
+		String[] arr = lower.split(" ");
+		for (String c : arr) {
+			if (c.equals("am")) {
+				count++;
+
 			}
-			if(char[i-1] == 'null' {
-				return 0;
 		}
-		
+		return count;
 	}
-	
-	//given a number 
+
+	// given a number
 	// if this number is divisible by 3 return "fizz"
 	// if this number is divisible by 5 return "buzz"
-	// if this number is divisible by both 3  and 5return "fizzbuzz"
+	// if this number is divisible by both 3 and 5return "fizzbuzz"
 	//
-	//fizzBuzz(3) → "fizz"
-	//fizzBuzz(10) → "buzz"
-	//fizzBuzz(15) → "fizzbuzz"
-	
+	// fizzBuzz(3) → "fizz"
+	// fizzBuzz(10) → "buzz"
+	// fizzBuzz(15) → "fizzbuzz"
+
 	public String fizzBuzz(int arg1) {
-	
-		
-		return null;
-		
-		
+		String c = "";
+		if (arg1 % 5 == 0 && arg1 % 3 == 0) {
+			return c = "fizzbuzz";
+		} else if (arg1 % 5 == 0) {
+			return c = "buzz";
+		} else if (arg1 % 3 == 0) {
+			return c = "fizz";
+		}
+		return c;
 	}
+		
+
 	
-	//Given a string split the string into the individual numbers present
-	//then add each digit of each number to get a final value for each number
+
+	// Given a string split the string into the individual numbers present
+	// then add each digit of each number to get a final value for each number
 	// String example = "55 72 86"
 	//
 	// "55" will = the integer 10
@@ -149,16 +169,13 @@ public class OfflineExercises {
 	//
 	// You then need to return the highest vale
 	//
-	//largest("55 72 86") → 14
-	//largest("15 72 80 164") → 11
-	//largest("555 72 86 45 10") → 15
-	
-	public int largest(String arg1) {
+	// largest("55 72 86") → 14
+	// largest("15 72 80 164") → 11
+	// largest("555 72 86 45 10") → 15
 
-	
-		
+	public int largest(String arg1) {
 		return 0;
+
 	}
-	
-	
+
 }
